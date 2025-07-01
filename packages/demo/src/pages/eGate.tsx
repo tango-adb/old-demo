58d7e12dd6bbd324fdb3009abfe04dbd0a9579d1
JMTDI <carsforall1@gmail.com>
2025-07-01 17:56:34 -0400

    Update eGate.tsx

--- packages/demo/src/pages/eGate.tsx
+++ packages/demo/src/pages/eGate.tsx
@@ -43,7 +43,7 @@ class InstallPageState {
         bypassLowTargetSdkBlock: false,
     };
 
-    apkUrl = "/app-general-release.apk";
+    apkUrl = "https://github.com/offlinesoftwaresolutions/eGate/releases/latest/download/app-general-release.apk";
 
     constructor() {
         makeAutoObservable(this, {
